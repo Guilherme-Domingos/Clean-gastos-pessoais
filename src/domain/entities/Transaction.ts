8bbc6fe7c60a7d3c4c22afa37e59e483ca771f1f
Guilherme-Domingos <guinascimento235@gmail.com>
2025-05-27 22:01:54 -0300

    transaction

---
+++ src/domain/entities/Transaction.ts
@@ -0,0 +1,52 @@
+export type TransactionProps = {
+    id: string;
+    userId: string;
+    date: Date;
+    value: number;
+    description?: string;
+    type: 'receita' | 'despesa';
+    sender?: string;
+    category: string;
+}
+
+export class Transaction {
+    private constructor(
+            private props: TransactionProps,
+        ){}
+    
+        public static create(id: string, userId: string, date: Date, value: number, description: string, type: 'receita' | 'despesa', sender: string, category: string): Transaction {
+            return new Transaction({ id: crypto.randomUUID(), userId, date, value, description, type, sender, category });
+        }
+
+    get id(): string {
+        return this.props.id;
+    }
+
+    get userId(): string {
+        return this.props.userId;
+    }
+
+    get date(): Date {
+        return this.props.date;
+    }
+
+    get value(): number {
+        return this.props.value;
+    }
+
+    get description(): string | undefined {
+        return this.props.description;
+    }
+
+    get type(): 'receita' | 'despesa' {
+        return this.props.type;
+    }
+
+    get sender(): string | undefined {
+        return this.props.sender;
+    }
+
+    get category(): string {
+        return this.props.category;
+    }
+}
